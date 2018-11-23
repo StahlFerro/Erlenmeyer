6756516137be6a17541f4673e90c537e15299aaf
StahlFerro <StahlFerro@users.noreply.github.com>
2018-11-24 00:17:48 +0700

    Added ship_status data to data.sql

--- data.sql
+++ data.sql
@@ -2,10 +2,10 @@
 -- PostgreSQL database dump
 --
 
--- Dumped from database version 9.6.11
--- Dumped by pg_dump version 9.6.11
+-- Dumped from database version 10.5
+-- Dumped by pg_dump version 10.5
 
--- Started on 2018-11-23 13:17:06 WIB
+-- Started on 2018-11-24 00:17:06
 
 SET statement_timeout = 0;
 SET lock_timeout = 0;
@@ -18,8 +18,8 @@ SET client_min_messages = warning;
 SET row_security = off;
 
 --
--- TOC entry 2213 (class 0 OID 386347)
--- Dependencies: 193
+-- TOC entry 2210 (class 0 OID 16542)
+-- Dependencies: 204
 -- Data for Name: builder; Type: TABLE DATA; Schema: public; Owner: postgres
 --
 
@@ -27,17 +27,8 @@ INSERT INTO public.builder (id, name, code, date_founded, founder, headquarters)
 
 
 --
--- TOC entry 2221 (class 0 OID 0)
--- Dependencies: 192
--- Name: builder_id_seq; Type: SEQUENCE SET; Schema: public; Owner: postgres
---
-
-SELECT pg_catalog.setval('public.builder_id_seq', 1, false);
-
-
---
--- TOC entry 2211 (class 0 OID 386332)
--- Dependencies: 191
+-- TOC entry 2208 (class 0 OID 16527)
+-- Dependencies: 202
 -- Data for Name: engine; Type: TABLE DATA; Schema: public; Owner: postgres
 --
 
@@ -45,17 +36,21 @@ INSERT INTO public.engine (id, name, code, power_output, type) VALUES (1, 'Westi
 
 
 --
--- TOC entry 2222 (class 0 OID 0)
--- Dependencies: 190
--- Name: engine_id_seq; Type: SEQUENCE SET; Schema: public; Owner: postgres
+-- TOC entry 2214 (class 0 OID 16571)
+-- Dependencies: 208
+-- Data for Name: ship_status; Type: TABLE DATA; Schema: public; Owner: postgres
 --
 
-SELECT pg_catalog.setval('public.engine_id_seq', 1, false);
+INSERT INTO public.ship_status (id, name) VALUES (1, 'Under construction');
+INSERT INTO public.ship_status (id, name) VALUES (2, 'Active');
+INSERT INTO public.ship_status (id, name) VALUES (3, 'Decommissioned');
+INSERT INTO public.ship_status (id, name) VALUES (4, 'Museum');
+INSERT INTO public.ship_status (id, name) VALUES (5, 'Scrapped');
 
 
 --
--- TOC entry 2215 (class 0 OID 498473)
--- Dependencies: 195
+-- TOC entry 2212 (class 0 OID 16557)
+-- Dependencies: 206
 -- Data for Name: ship_type; Type: TABLE DATA; Schema: public; Owner: postgres
 --
 
@@ -86,37 +81,19 @@ INSERT INTO public.ship_type (id, name) VALUES (24, 'Tanker');
 
 
 --
--- TOC entry 2209 (class 0 OID 386322)
--- Dependencies: 189
+-- TOC entry 2206 (class 0 OID 16517)
+-- Dependencies: 200
 -- Data for Name: ship; Type: TABLE DATA; Schema: public; Owner: postgres
 --
 
-INSERT INTO public.ship (id, name, code, speed, launch_date, capacity, engine_id, builder_id, ship_type_id) VALUES (1, 'USS Enterprise', 'CV-65', 40, '1961-01-01', 40, 1, NULL, 1);
-INSERT INTO public.ship (id, name, code, speed, launch_date, capacity, engine_id, builder_id, ship_type_id) VALUES (2, 'Queen Star 3', '9VBQ5', 65, '2016-10-28', 60, NULL, 1, 19);
-INSERT INTO public.ship (id, name, code, speed, launch_date, capacity, engine_id, builder_id, ship_type_id) VALUES (3, 'Pegasus V', 'P5STE', 31, '2013-05-12', 12, NULL, NULL, 19);
+INSERT INTO public.ship (id, name, code, speed, launch_date, capacity, engine_id, builder_id, ship_type_id, ship_status_id) VALUES (1, 'USS Enterprise', 'CV-65', 40, '1961-01-01', 40, 1, NULL, 1, NULL);
+INSERT INTO public.ship (id, name, code, speed, launch_date, capacity, engine_id, builder_id, ship_type_id, ship_status_id) VALUES (2, 'Queen Star 3', '9VBQ5', 65, '2016-10-28', 60, NULL, 1, 19, NULL);
+INSERT INTO public.ship (id, name, code, speed, launch_date, capacity, engine_id, builder_id, ship_type_id, ship_status_id) VALUES (3, 'Pegasus V', 'P5STE', 31, '2013-05-12', 12, NULL, NULL, 19, NULL);
 
 
 --
--- TOC entry 2223 (class 0 OID 0)
--- Dependencies: 188
--- Name: ship_id_seq; Type: SEQUENCE SET; Schema: public; Owner: postgres
---
-
-SELECT pg_catalog.setval('public.ship_id_seq', 1, false);
-
-
---
--- TOC entry 2224 (class 0 OID 0)
--- Dependencies: 194
--- Name: ship_type_id_seq; Type: SEQUENCE SET; Schema: public; Owner: postgres
---
-
-SELECT pg_catalog.setval('public.ship_type_id_seq', 1, false);
-
-
---
--- TOC entry 2207 (class 0 OID 386313)
--- Dependencies: 187
+-- TOC entry 2204 (class 0 OID 16508)
+-- Dependencies: 198
 -- Data for Name: user; Type: TABLE DATA; Schema: public; Owner: postgres
 --
 
@@ -124,15 +101,60 @@ INSERT INTO public."user" (id, username, password_hash) VALUES (1, 'admin', 'pbk
 
 
 --
+-- TOC entry 2220 (class 0 OID 0)
+-- Dependencies: 203
+-- Name: builder_id_seq; Type: SEQUENCE SET; Schema: public; Owner: postgres
+--
+
+SELECT pg_catalog.setval('public.builder_id_seq', 1, false);
+
+
+--
+-- TOC entry 2221 (class 0 OID 0)
+-- Dependencies: 201
+-- Name: engine_id_seq; Type: SEQUENCE SET; Schema: public; Owner: postgres
+--
+
+SELECT pg_catalog.setval('public.engine_id_seq', 1, false);
+
+
+--
+-- TOC entry 2222 (class 0 OID 0)
+-- Dependencies: 199
+-- Name: ship_id_seq; Type: SEQUENCE SET; Schema: public; Owner: postgres
+--
+
+SELECT pg_catalog.setval('public.ship_id_seq', 1, false);
+
+
+--
+-- TOC entry 2223 (class 0 OID 0)
+-- Dependencies: 207
+-- Name: ship_status_id_seq; Type: SEQUENCE SET; Schema: public; Owner: postgres
+--
+
+SELECT pg_catalog.setval('public.ship_status_id_seq', 1, false);
+
+
+--
+-- TOC entry 2224 (class 0 OID 0)
+-- Dependencies: 205
+-- Name: ship_type_id_seq; Type: SEQUENCE SET; Schema: public; Owner: postgres
+--
+
+SELECT pg_catalog.setval('public.ship_type_id_seq', 1, false);
+
+
+--
 -- TOC entry 2225 (class 0 OID 0)
--- Dependencies: 186
+-- Dependencies: 197
 -- Name: user_id_seq; Type: SEQUENCE SET; Schema: public; Owner: postgres
 --
 
 SELECT pg_catalog.setval('public.user_id_seq', 1, false);
 
 
--- Completed on 2018-11-23 13:17:06 WIB
+-- Completed on 2018-11-24 00:17:06
 
 --
 -- PostgreSQL database dump complete
